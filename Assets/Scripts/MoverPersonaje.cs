6bffbe4c866a55b4d08bde2aca1b1790a0e70d25
Dana <danae.ramirezrodriguez@gmail.com>
2026-03-21 11:58:47 -0600

    Generación de animaciones y transiciones
    Idle, Jumping y Walking

--- Assets/Scripts/MoverPersonaje.cs
+++ Assets/Scripts/MoverPersonaje.cs
@@ -3,36 +3,59 @@ using UnityEngine;
 public class MuevePersonaje : MonoBehaviour
 {
     public float velocidad = 5f;
-    public float fuerzaSalto = 7f;
+    public float fuerzaSalto = 8f;
 
     private Rigidbody2D rb;
+    private Animator animator;
     private bool enSuelo;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Movimiento horizontal
         float movimiento = Input.GetAxis("Horizontal");
+
+        // Movimiento horizontal
         rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
 
-        // Salto (solo si está en el suelo)
+        // Voltear a Mario según la dirección
+        if (movimiento > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (movimiento < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // Salto
         if (Input.GetKeyDown(KeyCode.Space) && enSuelo)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, fuerzaSalto);
-            enSuelo = false;
         }
+
+        // Actualizar Animator
+        animator.SetFloat("velocidad", Mathf.Abs(movimiento));
+        animator.SetBool("enSuelo", enSuelo);
     }
 
-    // Detectar si está tocando el suelo
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Suelo"))
         {
             enSuelo = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            enSuelo = false;
         }
     }
 }
